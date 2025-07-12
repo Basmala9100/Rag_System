7e4b435b669a763f680357c7064bb955dcecde03
Basmala9100 <bosybasmala603@gmail.com>
2025-07-12 14:22:56 +0300

    new update

--- web_rag.py
+++ web_rag.py
@@ -1,6 +1,23 @@
 import streamlit as st
+import psutil
+import time     
 from for_web import rag_pipeline
 
+def get_system_info():
+    st.sidebar.title("System Information: ")
+    
+    cpu_usage = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory()
+    memory_usage = memory.percent
+    disk = psutil.disk_usage('/')
+    
+    st.sidebar.markdown(f"CPU Usage: {cpu_usage}%")
+    st.sidebar.markdown(f"Memory Usage: {memory_usage}%")
+    st.sidebar.markdown(f"Disk Usage%: {disk.percent}%")
+    st.sidebar.markdown(f"Avaliable Memory: {round(memory.available / (1024 ** 3), 2)} GB")
+    st.sidebar.markdown(f"Used Memory: {round(memory.used / (1024 ** 3), 2)} GB")
+    
+    
 
 st.set_page_config(page_title="RAG Q&A", layout="centered")
 st.title("RAG Chat")
@@ -19,4 +36,5 @@ def main():
 
 
 if __name__ == "__main__":
+    get_system_info()
     main()
